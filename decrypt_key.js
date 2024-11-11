ddb1ab1db7178e50e90d2a4a3e3b9a0b59353025
Aathish Sivasubrahmanian <aathish04@gmail.com>
2024-11-11 19:27:24 +0530

    Clean up processing workflow.

--- decrypt_key.js
+++ decrypt_key.js
@@ -14,27 +14,40 @@ d = n.enc
 h = d.Hex.parse
 c = n.pad.NoPadding;
 apkId = process.argv[2]
+timestampdata = process.argv[3]
+timestampurl = process.argv[4]
 
-var f = new Uint8Array(eval(process.argv[3]))
-    , g = f.subarray(16, 32)
-    , v = f.subarray(48, 64);
+var f = new Uint8Array(eval(timestampdata))
+var pp = timestampurl.split('/timestamp/')[1] ?? '',
+        v = f.subarray(8, 24),
+        g = f.subarray(32, 48);
+    if (pp === 'aav') {
+        v = f.subarray(16, 32); 
+        g = f.subarray(48, 64);
+      } else if (pp === 'scw') {
+        v = f.subarray(0, 16); 
+        g = f.subarray(48, 64);
+      } else if (pp === 'scs') {
+        v = f.subarray(8, 24); 
+        g = f.subarray(32, 48);
+      } else if (pp === 'sdq') {
+        g = f.subarray(0, 16); 
+        v = f.subarray(48, 64);
+      } else if (pp === 'sxc') {
+        v = f.subarray(0, 16); 
+        g = f.subarray(32, 48);
+      } else if (pp === 'q1wq') {
+        g = f.subarray(0, 16); 
+        v = f.subarray(32, 48);
+      } else if (pp === 'w1q') {
+        g = f.subarray(16, 32); 
+        v = f.subarray(48, 64);
+      }
 
-
-f = a(o(g), h(apkId.substring(0, 5) + apkId.substring(37)), {
-    mode: u,
-    padding: c
-})
-
-// f = a(o(g), h(apkId.substring(0, 16) + apkId.substring(48)), {
-//     mode: u,
-//     padding: c
-// })
-
-
-// f = a(o(g), h(apkId.substring(32)), {
-//     mode: u,
-//     padding: c
-// }),
+f = a(o(g), h(apkId.substring(16, 48)), {
+        mode: u,
+        padding: c,
+      })
 
 f = convert(a(o(v), f, {
     mode: u,
